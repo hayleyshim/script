801aecaee3a68849f5a5a2bb551d057015c75392
hayleyshim <yhshim17@gmail.com>
2022-01-13 17:16:35 +0900

    office

---
+++ datatype2.py
@@ -0,0 +1,23 @@
+nft = 100000000
+
+bitcoin = 9744000
+ripple = 711
+
+total = bitcoin*10 + ripple*430
+print(total)
+
+hold1 = ["bitcoin", "ripple", "ethereum"]
+
+hold2 = ["btc_krw", "xrp_krw", "eth_krw"]
+
+print(hold1[0])
+
+hold1 = "bitcoin"
+hold2 = "ripple"
+hold3 = "ethereum"
+
+portfolio = ["BTC", "ETH", "XRP", "BCH", "DASH"]
+print(portfolio[0:3])
+
+portfolio = []
+portfolio.append
