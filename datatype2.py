37a332885b0f2fa7441a2f823a361565015e6cc4
hayleyshim <yhshim17@gmail.com>
2022-01-17 16:05:52 +0900

    main

---
+++ datatype2.py
@@ -0,0 +1,35 @@
+a = 123
+a = -178
+a = 0
+
+a = 1.2
+a = -3.45
+
+a = 4.24E10
+a = 4.24e-10
+
+a = 0o177
+a = 0x8ff
+b = 0xABC
+
+a = 3
+b = 4
+c = a + b
+print(c)
+c = a * b
+print(c)
+c = a/b
+print(c)
+c = a**b
+print(c)
+
+c = 7%3
+print(c)
+c = 3%7
+print(c)
+
+c = 7/4
+print(c)
+
+c = 7//4
+print(c)
