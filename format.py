80bb1acc3507d38c93676b843b3e22fb17b3afc6
hayleyshim <yhshim17@gmail.com>
2022-01-17 21:59:12 +0900

    formatting

---
+++ format.py
@@ -0,0 +1,15 @@
+print('{}, my name is yh'.format('hello'))
+str1 = 'this is yh, and I am studying {} scripting language!'.format('python')
+print(str1.format('python'))
+str2 = 'this is Shim {}. I work at {} Resource department. I am {} years old'
+print(str2.format('yonghui', 'Human', '32'))
+
+print('_________________________________________')
+
+a = 10
+b = 20
+print('the value of a and b are %d %d' %(a, b))
+
+c = a + b
+print('sum of a and b are %d ' %(c))
+
---
+++ std.py
@@ -0,0 +1,18 @@
+import sys
+
+print('enter number1: ')
+a = int(sys.stdin.readline())
+
+print('enter the number2: ')
+b = int(sys.stdin.readline())
+
+c = a + b
+sys.stdout.write('the Result is: %d' %c)
+
+str1 = input('Enter a String: ')
+print('Entered String is ', str1)
+
+a = int(input('enter the value for a: '))
+b = int(input('enter the value for b: '))
+c = a + b
+print('the value for c is : ', c)
