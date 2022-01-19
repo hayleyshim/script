daea17f12ad4474ae12cbcc8ed60df32ba51349c
hayleyshim <yhshim17@gmail.com>
2022-01-20 01:01:33 +0900

    mysql edit

--- mysql/connect.py
+++ mysql/connect.py
@@ -5,7 +5,7 @@ con = mysql.connector.connect(user='root', password='', host='127.0.0.1', databa
 cursor = con.cursor()
 print('connected!')
 print('inserting...')
-cursor.execute('INSERT INTO student VALUES(2,\'yonghui\',\'shim\',\'yonghuishim@gmail.com\',\'+1050693672\')')
+cursor.execute('INSERT INTO student VALUES(2,\'yonghui\',\'shim\',\'yonghuishim@gmail.com\',\'+10506123123\')')
 con.commit()
 print('inserted!')
 con.close()
